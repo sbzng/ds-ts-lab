5d1d513578ae362c046ebe151bc5fbd6e5e7c8f6
Shaobo Zang <20105527@mail.wit.ie>
2024-01-23 17:34:32 +0000

    Demo optional properties and parameters.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -38,16 +38,22 @@ console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
 function sortColleagues(
     colleagues: Colleague[],
-    sorter: (c1: Colleague, c2: Colleague) => number
-): EmailContact[] {
-    const sorted = colleagues.sort(sorter); // Colleague[] inferred
-    const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
-    return result
-}
-
-console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
-console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
-
+    sorter: (c1: Colleague, c2: Colleague) => number,
+    max? : number
+  ): EmailContact[] {
+    let end = colleagues.length;
+    if (max !== undefined) {
+       end = max < 2 ? 1 : max
+    }
+    const sorted = colleagues.sort(sorter);
+    const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+    return fullResult.slice(0,end)
+  }
+  // Test invocations
+  console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
+  console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+  console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
+  
 function findFriends(friends: Friend[], criterion: (f: Friend) => boolean) {
     return friends.filter(criterion).map(f => f.name);
 }
